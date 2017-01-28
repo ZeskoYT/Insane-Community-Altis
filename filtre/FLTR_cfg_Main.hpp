492354d5b8fcc4424e4cdeaf24cece60f5de741d
Zesko <zskglitcher@gmail.com>
2017-01-28 21:47:16 +0100

    Filtre de map By BlayFixe
    Vous ne pourrez pas toucher sans le life_server :)

---
+++ filtre/FLTR_cfg_Main.hpp
@@ -0,0 +1,11 @@
+class markerGroups
+{
+	class banane
+	{
+		displayName = "Champs de bananes";
+		mrkrNames[] = {"banane_1"};
+		mrkrTypes[] = {};
+		mrkrTypeColors[] = {};
+		side = "Default";
+	};
+};
